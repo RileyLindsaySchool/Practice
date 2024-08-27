d7f5bd3b482c2ecea510ccd8609983272fbf7a42
riley <rlindsay@atu.edu>
2024-08-27 10:47:01 -0500

    Work work work

--- GitHubTestV2/GitHubTestV2.cpp
+++ GitHubTestV2/GitHubTestV2.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main()
 {
 	cout << "Hello, GitHub!" << endl;
+	cout << "This is the 1st commit!" << endl;
+	cout << "Just work already!" << endl;
+	cout << "Please commit this!" << endl;
 
 	return 0;
 }
